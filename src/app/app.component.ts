9171adef2f762f000fa5d5eb60337ceabadc30fc
Osman Jimenez <osmanjimenez.dev@gmail.com>
2022-08-21 14:25:38 -0500

    Feat: enable force update pwa #1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private swUpdate: SwUpdate, private platform: Platform) {
+    this.updatePWA();
+  }
+
+  installEvent: any = null;
+
+  ngOnInit(): void {
+    window.addEventListener('beforeinstallprompt', (e) => {
+      console.log(`'beforeinstallprompt' event was fired.`, e);
+      this.installEvent = e;
+
+      if (this.platform.is('desktop')) {
+        this.installByUser();
+      }
+    });
+  }
+
+  installByUser() {
+    console.log(this.installEvent);
+    if (this.installEvent) {
+      this.installEvent.prompt();
+      this.installEvent.userChoice.then((rta: any) => {
+        if (rta.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+      });
+    }
+  }
+
+  updatePWA() {
+    this.swUpdate.available.subscribe((value) => {
+      console.log('update', value);
+      window.location.reload();
+    });
+  }
 }
