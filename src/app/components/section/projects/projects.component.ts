37e4ab63f49f37efe694bcf4114d06823de31253
Osman Jimenez <osmanjimenez.dev@gmail.com>
2022-09-30 09:41:02 -0500

    Feat: optimized images #8

--- src/app/components/section/projects/projects.component.ts
+++ src/app/components/section/projects/projects.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectsComponent implements OnInit {
   projects = [
     {
-      url_photo: 'FusaTour.jpg',
+      url_photo: 'FusaTour.webp',
       name: 'FusaTour',
       url: 'https://github.com/OsmanJimenez/FusaTour',
       skills: [
@@ -22,37 +22,37 @@ export class ProjectsComponent implements OnInit {
       ],
     },
     {
-      url_photo: 'Template_Portafolio.png',
+      url_photo: 'Template_Portafolio.webp',
       name: 'Template Portafolio',
       url: 'https://osmanjimenez.github.io/Template_Portafolio/',
       skills: ['Bootstrap', 'HTML5', 'JavaScript', 'Material Design'],
     },
     {
-      url_photo: 'FreskyPan.png',
+      url_photo: 'FreskyPan.webp',
       name: 'FreskyPan ERP',
       url: 'https://github.com/OsmanJimenez/FreskyPan',
       skills: ['Bootstrap', 'HTML5', 'JavaScript', 'MySQL', 'PHP'],
     },
     {
-      url_photo: 'Template_Restaurante.png',
+      url_photo: 'Template_Restaurante.webp',
       name: 'Template Restaurante',
       url: 'https://osmanjimenez.github.io/Plantilla-Restaurante/',
       skills: ['Bootstrap', 'HTML5', 'JavaScript'],
     },
     {
-      url_photo: 'Realidad_Virtual.png',
+      url_photo: 'Realidad_Virtual.webp',
       name: 'Realidad Virtual',
       url: 'https://github.com/OsmanJimenez?tab=repositories&q=Aframe&type=&language=&sort=',
       skills: ['A-Frame', 'HTML5', 'CSS3', 'JavaScript'],
     },
     {
-      url_photo: 'Template_FlexBox.png',
+      url_photo: 'Template_FlexBox.webp',
       name: 'Template Flexbox',
       url: 'https://osmanjimenez.github.io/Template-Flexbox/',
       skills: ['Bootstrap', 'HTML5', 'FlexBpx'],
     },
     {
-      url_photo: 'Perceptron.png',
+      url_photo: 'Perceptron.webp',
       name: 'Perceptron',
       url: 'https://github.com/OsmanJimenez?tab=repositories&q=percep&type=&language=&sort=',
       skills: [
@@ -66,7 +66,7 @@ export class ProjectsComponent implements OnInit {
       ],
     },
     {
-      url_photo: 'OWASP_PHP.png',
+      url_photo: 'OWASP_PHP.webp',
       name: 'OWASP PHP',
       url: 'https://github.com/OsmanJimenez/OWASP_PHP',
       skills: [
@@ -80,7 +80,7 @@ export class ProjectsComponent implements OnInit {
       ],
     },
     {
-      url_photo: 'Template_Material_Design.png',
+      url_photo: 'Template_Material_Design.webp',
       name: 'Template Material Design',
       url: 'https://osmanjimenez.github.io/Template-for-Blog-Material-Design/',
       skills: ['Bootstrap', 'HTML5', 'Material Design'],
