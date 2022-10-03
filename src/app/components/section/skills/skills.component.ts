f3cb498d123bdba77675baf46e754d5f7a7ff9af
Osman Jimenez <osmanjimenez.dev@gmail.com>
2022-10-03 14:04:20 -0500

    Feat: add new skills #2

--- src/app/components/section/skills/skills.component.ts
+++ src/app/components/section/skills/skills.component.ts
@@ -8,32 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class SkillsComponent implements OnInit {
   skills = [
     {
+      icon: 'devicon-bash-plain colored',
+      name: '.Env',
+    },
+    {
+      icon: 'devicon-ansible-plain colored',
+      name: 'A-Frame',
+    },
+    {
       icon: 'devicon-angularjs-plain colored',
       name: 'Angular',
     },
     {
-      icon: 'devicon-dotnetcore-plain colored',
-      name: '.Net',
+      icon: 'devicon-fsharp-plain colored',
+      name: 'Ant Design',
     },
     {
-      icon: 'devicon-html5-plain colored',
-      name: 'HTML 5',
+      icon: 'devicon-bootstrap-plain colored',
+      name: 'Bootstrap',
+    },
+    {
+      icon: 'devicon-embeddedc-plain colored',
+      name: 'Capacitor',
+    },
+    {
+      icon: 'devicon-csharp-plain colored',
+      name: 'C#',
     },
     {
       icon: 'devicon-css3-plain colored',
-      name: 'CSS 3',
+      name: 'CSS3',
     },
     {
-      icon: 'devicon-javascript-plain colored',
-      name: 'JavaScript',
-    },
-    {
-      icon: 'devicon-typescript-plain colored',
-      name: 'TypeScript',
-    },
-    {
-      icon: 'devicon-microsoftsqlserver-plain colored',
-      name: 'Sql Server',
+      icon: 'devicon-html5-plain colored',
+      name: 'HTML5',
     },
     {
       icon: 'devicon-git-plain colored',
@@ -44,8 +52,40 @@ export class SkillsComponent implements OnInit {
       name: 'Ionic',
     },
     {
-      icon: 'devicon-csharp-plain colored',
-      name: 'C#',
+      icon: 'devicon-javascript-plain colored',
+      name: 'JavaScript',
+    },
+    {
+      icon: 'devicon-laravel-plain colored',
+      name: 'Laravel',
+    },
+    {
+      icon: 'devicon-markdown-original colored',
+      name: 'Markdown',
+    },
+    {
+      icon: 'devicon-microsoftsqlserver-plain colored',
+      name: 'Microsoft Sql Server',
+    },
+    {
+      icon: 'devicon-mysql-plain colored',
+      name: 'MySql',
+    },
+    {
+      icon: 'devicon-dotnetcore-plain colored',
+      name: '.Net',
+    },
+    {
+      icon: 'devicon-python-plain colored',
+      name: 'Python',
+    },
+    {
+      icon: 'devicon-shopware-original colored',
+      name: 'PWA',
+    },
+    {
+      icon: 'devicon-typescript-plain colored',
+      name: 'TypeScript',
     },
   ];
 
