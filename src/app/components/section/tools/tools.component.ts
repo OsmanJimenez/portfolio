74c13c6e8faae2bfe6d1fd744896e1731fa5ddc4
OsmanJimenez <osmanjimenez.dev@gmail.com>
2023-08-19 15:31:49 -0500

    Feat: create service tools #12

--- src/app/components/section/tools/tools.component.ts
+++ src/app/components/section/tools/tools.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToolsService } from 'src/app/services/section/tools/tools.service';
 
 @Component({
   selector: 'app-tools',
@@ -6,79 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tools.component.scss'],
 })
 export class ToolsComponent implements OnInit {
-  tools = [
-    {
-      icon: '',
-      src: 'adobe-xd.svg',
-      name: 'Adobe XD',
-    },
-    {
-      icon: '',
-      src: 'bitbucket.svg',
-      name: 'Bitbuquet',
-    },
-    {
-      icon: '',
-      src: 'clicdata-logo.webp',
-      name: 'Clic Data',
-    },
-    {
-      icon: '',
-      src: 'draw-io.png',
-      name: 'Draw.io',
-    },
-    {
-      icon: '',
-      src: 'figma.svg',
-      name: 'Figma',
-    },
-    {
-      icon: 'devicon-jira-plain colored',
-      src: '',
-      name: 'Jira',
-    },
-    {
-      icon: '',
-      src: 'gitlab.svg',
-      name: 'GitLab',
-    },
-    {
-      icon: '',
-      src: 'github.svg',
-      name: 'GitHub',
-    },
-    {
-      icon: '',
-      src: 'sql-server.webp',
-      name: 'SQL Server',
-    },
-    {
-      icon: 'devicon-wordpress-plain colored',
-      src: '',
-      name: 'Wordpress',
-    },
-    {
-      icon: 'devicon-windows8-original colored',
-      src: '',
-      name: 'Windows',
-    },
-    {
-      icon: 'devicon-ubuntu-plain colored',
-      src: '',
-      name: 'Ubuntu',
-    },
-    {
-      icon: '',
-      src: 'postman.svg',
-      name: 'Postman',
-    },
-    {
-      icon: 'devicon-vscode-plain colored',
-      src: '',
-      name: 'VSCode',
-    },
-  ];
-  constructor() {}
+  tools = []
+  constructor(private toolsService: ToolsService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.loadTools()
+  }
+
+  loadTools() {
+    this.toolsService.get().subscribe(
+      res => {
+        if (res) {
+          this.tools = res;
+        }
+      }
+    );
+  }
 }
--- src/app/services/section/tools/tools.service.ts
+++ src/app/services/section/tools/tools.service.ts
@@ -1,18 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import mockSkills from 'src/app/mocks/skills.mock.json';
+import mockTools from 'src/app/mocks/tools.mock.json';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SkillsService {
+export class ToolsService {
 
   constructor() { }
 
   get(): Observable<any> {
     // Simula una petición HTTP y devuelve el mock JSON
-    return of(mockSkills).pipe(
+    return of(mockTools).pipe(
       catchError(error => {
         console.error('Error fetching certificate:', error);
         return of(null);
