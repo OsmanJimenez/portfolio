5bf7c197bc521232886d4868abfbc64fce5278dc
OsmanJimenez <osmanjimenez.dev@gmail.com>
2024-01-14 13:04:57 -0500

    Chore: update version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,9 +41,19 @@ export class AppComponent implements OnInit {
   }
 
   updatePWA() {
-    this.swUpdate.versionUpdates.subscribe((value) => {
-      console.log('update', value);
-      window.location.reload();
+    this.swUpdate.versionUpdates.subscribe(evt => {
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version: ${evt.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`Current app version: ${evt.currentVersion.hash}`);
+          console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+          break;
+      }
     });
   }
 }
